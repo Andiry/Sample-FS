62fc23fa6d566db38d94c90483c0d5b6a9a9e9c3
Andiry Xu <andiry.xu@gmail.com>
2013-10-28 20:58:00 -0700

    Init commit for sample FS

---
+++ day1/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_/home/brienza/test/sample_fs/samplefs/day1/built-in.o :=  rm -f /home/brienza/test/sample_fs/samplefs/day1/built-in.o; ar rcsD /home/brienza/test/sample_fs/samplefs/day1/built-in.o
